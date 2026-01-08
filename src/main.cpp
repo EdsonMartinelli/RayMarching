d860e224bc40042f21b02c31d9047fe4133e9179
Edson Martinelli <martinelli1997@gmail.com>
2026-01-08 01:40:48 -0300

    fix: change frag shader path in main

--- src/main.cpp
+++ src/main.cpp
@@ -169,7 +169,7 @@ int main() {
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     unsigned int vertexShader = createShader(GL_VERTEX_SHADER, "src/vertexshader.vert");
-    unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, "src/UFABCPhongHardShadowTeste.frag");
+    unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, "src/UFABCFull3D.frag");
     unsigned int shaderProgram = createShaderProgram(vertexShader, fragmentShader); 
 
     float vertices[] = {
